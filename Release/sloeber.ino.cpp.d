65a640b03388ebf5ae1910d06cbec29b376a8752
Ruben <rubenbg@gmail.com>
2018-05-10 08:52:58 +0200

    Primer commit

---
+++ Release/sloeber.ino.cpp.d
@@ -0,0 +1,43 @@
+sloeber.ino.cpp.o: ../sloeber.ino.cpp \
+ /Applications/sloeber.app/Contents/Eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.21/cores/arduino/Arduino.h \
+ /Applications/sloeber.app/Contents/Eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.21/cores/arduino/binary.h \
+ /Applications/sloeber.app/Contents/Eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.21/cores/arduino/WCharacter.h \
+ /Applications/sloeber.app/Contents/Eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.21/cores/arduino/WString.h \
+ /Applications/sloeber.app/Contents/Eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.21/cores/arduino/HardwareSerial.h \
+ /Applications/sloeber.app/Contents/Eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.21/cores/arduino/Stream.h \
+ /Applications/sloeber.app/Contents/Eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.21/cores/arduino/Print.h \
+ /Applications/sloeber.app/Contents/Eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.21/cores/arduino/Printable.h \
+ /Applications/sloeber.app/Contents/Eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.21/cores/arduino/USBAPI.h \
+ /Applications/sloeber.app/Contents/Eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.21/cores/arduino/Arduino.h \
+ /Applications/sloeber.app/Contents/Eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.21/variants/eightanaloginputs/pins_arduino.h \
+ /Applications/sloeber.app/Contents/Eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.21/variants/eightanaloginputs/../standard/pins_arduino.h \
+ /Applications/sloeber.app/Contents/Eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.21/cores/arduino/Adafruit_NeoPixel.h \
+ ../RuboDaftPunkCore1.ino
+
+/Applications/sloeber.app/Contents/Eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.21/cores/arduino/Arduino.h:
+
+/Applications/sloeber.app/Contents/Eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.21/cores/arduino/binary.h:
+
+/Applications/sloeber.app/Contents/Eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.21/cores/arduino/WCharacter.h:
+
+/Applications/sloeber.app/Contents/Eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.21/cores/arduino/WString.h:
+
+/Applications/sloeber.app/Contents/Eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.21/cores/arduino/HardwareSerial.h:
+
+/Applications/sloeber.app/Contents/Eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.21/cores/arduino/Stream.h:
+
+/Applications/sloeber.app/Contents/Eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.21/cores/arduino/Print.h:
+
+/Applications/sloeber.app/Contents/Eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.21/cores/arduino/Printable.h:
+
+/Applications/sloeber.app/Contents/Eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.21/cores/arduino/USBAPI.h:
+
+/Applications/sloeber.app/Contents/Eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.21/cores/arduino/Arduino.h:
+
+/Applications/sloeber.app/Contents/Eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.21/variants/eightanaloginputs/pins_arduino.h:
+
+/Applications/sloeber.app/Contents/Eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.21/variants/eightanaloginputs/../standard/pins_arduino.h:
+
+/Applications/sloeber.app/Contents/Eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.21/cores/arduino/Adafruit_NeoPixel.h:
+
+../RuboDaftPunkCore1.ino:
---
+++ sloeber.ino.cpp
@@ -0,0 +1,17 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2018-05-09 22:17:59
+
+#include "Arduino.h"
+#include <Adafruit_NeoPixel.h>
+
+void setup() ;
+void loop() ;
+void MatrizPrincipalComplete();
+
+#include "RuboDaftPunkCore1.ino"
+
+
+#endif
