c4e6dcfbdf6cee75a18fd7223c8311435fecd255
Ruben <rubenbg@gmail.com>
2018-05-23 16:59:35 +0200

    Commit

--- sloeber.ino.cpp
+++ sloeber.ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2018-05-14 22:15:25
+//This file has been generated on 2018-05-23 16:58:07
 
 #include "Arduino.h"
 #include <Wire.h>
